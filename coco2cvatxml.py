cb368cca7860ae6d6ae1009d7662848313ca1d22
Reggievw <reggievanwoensel@yahoo.com>
2022-04-27 22:50:03 +0200

    added line comments

--- coco2cvatxml.py
+++ coco2cvatxml.py
@@ -2,13 +2,12 @@
 Convert from COCO JSON to CVAT XML
 """
 
+
 import json
 import sys
 import sys
-import getopt
+import argparse
 
-from xml.sax import saxutils
-from io import StringIO
 from collections import OrderedDict
 from xml.sax.saxutils import XMLGenerator
 
@@ -82,11 +81,6 @@ class XmlAnnotationWriter:
         self.xmlgen.startElement("box", box)
         self._level += 1
 
-    def open_polygon(self, polygon):
-        self._indent()
-        self.xmlgen.startElement("polygon", polygon)
-        self._level += 1
-
     def open_polyline(self, polyline):
         self._indent()
         self.xmlgen.startElement("polyline", polyline)
@@ -95,11 +89,6 @@ class XmlAnnotationWriter:
     def open_points(self, points):
         self._indent()
         self.xmlgen.startElement("points", points)
-        self._level += 1
-
-    def open_cuboid(self, cuboid):
-        self._indent()
-        self.xmlgen.startElement("cuboid", cuboid)
         self._level += 1
 
     def open_tag(self, tag):
@@ -118,11 +107,6 @@ class XmlAnnotationWriter:
         self._indent()
         self.xmlgen.endElement("box")
 
-    def close_polygon(self):
-        self._level -= 1
-        self._indent()
-        self.xmlgen.endElement("polygon")
-
     def close_polyline(self):
         self._level -= 1
         self._indent()
@@ -132,11 +116,6 @@ class XmlAnnotationWriter:
         self._level -= 1
         self._indent()
         self.xmlgen.endElement("points")
-
-    def close_cuboid(self):
-        self._level -= 1
-        self._indent()
-        self.xmlgen.endElement("cuboid")
 
     def close_tag(self):
         self._level -= 1
@@ -160,11 +139,6 @@ class XmlAnnotationWriter:
         self.xmlgen.endDocument()
 
 
-def pairwise(iterable):
-    a = iter(iterable)
-    return zip(a, a)
-
-
 def threewise(iterable):
     a = iter(iterable)
     return zip(a, a, a)
@@ -175,66 +149,59 @@ def fourwise(iterable):
     return zip(a, a, a, a)
 
 
-def fivewise(iterable):
-    a = iter(iterable)
-    return zip(a, a, a, a, a)
+def convert(jsonFile, xmlFile, withBodyKeyPoints):
+    """
+    Transforms COCO json to an XML in CVAT format.
+    """
+    # bodykeypoint names
+    body_key_labels = ["nose", "", "", "", "", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist",
+                       "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle"]
 
-
-def main(argv):
-    withKeyPoints = True
-    inputfile = ''
-    outputfile = ''
-    try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-    except getopt.GetoptError:
-        print('coco2cvatxml.py -i <inputfile> -o <outputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('coco2cvatxml.py -i <inputfile> -o <outputfile>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-
-    with open(outputfile, 'w') as f:
+    # Write the xml file
+    with open(xmlFile, 'w') as f:
         dumper = XmlAnnotationWriter(f)
         dumper.open_root()
-        key_labels = ["nose", "", "", "", "", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist",
-                      "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle"]
-        frame_no = 0
-        point_track_id = 0
-        # Opening JSON file
-        json_f = open(inputfile)
-        track_ids = []
+
+        # Opening the JSON file
+        json_f = open(jsonFile)
+        track_ids_to_convert = []
         json_data = json.load(json_f)
         for data in json_data["annotations"]:
             track_id = data["track_id"]
-            if track_id not in track_ids:
-                track_ids.append(track_id)
-        for people_no in track_ids:
+            if track_id not in track_ids_to_convert:
+                track_ids_to_convert.append(track_id)
+
+        xml_track_id = 0
+        # Loop through json_data according to track_id (ID remains constant for that person/object in all the sequences)
+        for track_id_to_convert in track_ids_to_convert:
             max_frame_id = 0
             for data in json_data["annotations"]:
                 track_id = data["track_id"]
                 frame_id = data["frame_id"]
-                if people_no != track_id:
+                # if track ID different => not the person/object to convert in this loop
+                if track_id_to_convert != track_id:
                     continue
+                # set max frame_id
                 if frame_id > max_frame_id:
                     max_frame_id = frame_id
+
             track = {
-                'id': str(point_track_id),
+                'id': str(xml_track_id),
                 'label': 'person',
-                'group_id': str(people_no)
+                'group_id': str(track_id_to_convert)
             }
+
             dumper.open_track(track)
-            point_track_id += 1
+            # Add 1 to track for next object to convert
+            xml_track_id += 1
+
+            # Convert bounding box to XML
             for data in json_data["annotations"]:
                 track_id = data["track_id"]
-                if people_no != track_id:
+                # if track ID different => not the person/object to convert in this loop
+                if track_id_to_convert != track_id:
                     continue
                 box = data["bbox"]
-               # for a,b,c,d, z in fivewise(box):
                 for a, b, c, d in fourwise(box[:4]):
                     shape = OrderedDict()
                     shape.update(OrderedDict([
@@ -249,43 +216,47 @@ def main(argv):
                     shape["outside"] = str(0)
                     shape["occluded"] = str(0)
                     shape["z_order"] = str(0)
-                    # if frame_no == 0 or frame_no % 5 ==0:
+                    # if frame_no % x ==0: -> keyframe every x frames (use interpolation in between)
                     shape["keyframe"] = str(1)
                     if frame_no == max_frame_id:
                         shape["outside"] = str(1)
-                        #shape["keyframe"] = str(1)
                     dumper.open_box(shape)
                     dumper.close_box()
             dumper.close_track()
-            # Body key points to XML
-            if(withKeyPoints):
-                # points
-                for point_no in range(17):
-                    if (point_no > 0 and point_no < 5):
+
+            # Convert body key points to XML
+            if(withBodyKeyPoints):
+                # Iterate over all body parts
+                for bodykey_idx in range(len(body_key_labels)):
+                    # Don't convert pointkey from the face
+                    if (bodykey_idx > 0 and bodykey_idx < 5):
                         continue
+                    # Create a track for each key point in the XML
                     track = {
-                        'id': str(point_track_id),
-                        'label': key_labels[point_no],
-                        'group_id': str(people_no)
+                        'id': str(xml_track_id),
+                        'label': body_key_labels[bodykey_idx],
+                        'group_id': str(track_id_to_convert)
                     }
                     dumper.open_track(track)
-                    point_track_id += 1
-                    index = 0
+                    # Add 1 to track for next object to convert
+                    xml_track_id += 1
                     for data in json_data["annotations"]:
                         track_id = data["track_id"]
-                        if people_no != track_id:
+                        if track_id_to_convert != track_id:
                             continue
                         frame_no = data["frame_id"]
-                        key_point_no = 0
+                        xml_point_idx = 0
                         for keypoint in data["keypoints"]:
+                            # x and y indicate pixel positions in the image. z indicates conficence
                             for x, y, z in threewise(keypoint):
-                                if key_point_no != point_no:
+                                # if point idx different => not the bodypart to convert in this loop
+                                if xml_point_idx != bodykey_idx:
                                     continue
-                                # dumper.open_track(track)
                                 shape = OrderedDict()
                                 shape["frame"] = str(frame_no)
                                 shape["outside"] = str(0)
                                 shape["keyframe"] = str(1)
+                                # if last point from sequence or confidence under threshold, put outside = 1
                                 if frame_no == max_frame_id or z < 0.4:
                                     shape["outside"] = str(1)
                                 else:
@@ -296,15 +267,35 @@ def main(argv):
                                     {"points": '{:.2f},{:.2f}'.format(x, y)})
                                 dumper.open_points(shape)
                                 dumper.close_points()
-                            key_point_no += 1
-                        index += 1
+                            xml_point_idx += 1
                     dumper.close_track()
-                    point_no += 1
+                    bodykey_idx += 1
         # Closing file
         json_f.close()
         dumper.close_root()
 
+def parse_args():
+    """Parse arguments of command line"""
+    parser = argparse.ArgumentParser(
+        description='Convert CVAT XML annotations to COCO JSON format'
+    )
+    parser.add_argument(
+        '--cvat-xml', metavar='FILE', required=True,
+        help='input file with CVAT annotation in xml format'
+    )
+    parser.add_argument(
+        '--coco', metavar='FILE', required=True,
+        help='FILE for output annotations in JSON format'
+    )
+    return parser.parse_args()
+
+
+def main():
+    withBodyKeyPoints = True
+    args = parse_args()
+    convert(args.coco, args.cvat_xml, withBodyKeyPoints)
+
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
 # print(stream.getvalue())
--- cvatxml2coco.py
+++ cvatxml2coco.py
@@ -4,13 +4,11 @@ Convert from CVAT XML to COCO JSON
 
 import xml.etree.ElementTree as ET
 import json
-import sys
-import getopt
+import argparse
 
 from datetime import datetime, date
 from pathlib import Path
 from PIL import Image
-#from tqdm import tqdm
 from cocojson.utils.common import path, get_imgs_from_dir
 
 
@@ -20,31 +18,16 @@ def write_json(json_path, dic):
     print(f"Wrote json to {json_path}")
 
 
-def main(argv):
-    inputfile = ''
-    outputfile = ''
-    img_root = None
-    try:
-        opts, args = getopt.getopt(
-            argv, "hi:o:m:", ["ifile=", "ofile=", "mdir="])
-    except getopt.GetoptError:
-        print('cvatxml2coco.py -i <inputfile> -o <outputfile> -m <imageDir>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('cvatxml2coco.py -i <inputfile> -o <outputfile> -m <imageDir>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-        elif opt in ("-m", "--mdir"):
-            img_root = arg
-
-    tree = ET.parse(inputfile)
+def convert(xmlfile, img_root, outputfile):
+    """
+    Transforms an XML in CVAT format to COCO.
+    """
+    # Parse XML
+    tree = ET.parse(xmlfile)
     root = tree.getroot()
-    meta = root.find("meta")
 
+    # META info
+    meta = root.find("meta")
     task = meta.find("task")
     taskname = task.find("name")
     if taskname is None:
@@ -55,7 +38,7 @@ def main(argv):
     if outputfile:
         out_json = outputfile
     else:
-        json_file_name = taskname if taskname else Path(inputfile).stem
+        json_file_name = taskname if taskname else Path(xmlfile).stem
         out_json = f"{json_file_name}.json"
 
     dumped = meta.find("dumped")
@@ -78,6 +61,7 @@ def main(argv):
     else:
         stop_frame = int(stop_frame.text)
 
+    # A collection of “info”, “images”, “annotations”, “categories”
     coco_dict = {
         "info": {"description": taskname, "data_created": date_str},
         "annotations": [],
@@ -85,12 +69,20 @@ def main(argv):
         "images": [],
     }
 
+    # bodykeypoint names
     key_body_labels = ["nose", "head_bottom", "head_top", "left_ear", "right_ear", "left_shoulder", "right_shoulder",  "left_elbow",
                        "right_elbow",  "left_wrist", "right_wrist", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle"]
 
-    labels = meta.find("task").find("labels")
-    this_cat_id = 2
+    # The “categories” object contains a list of categories (e.g. dog, boat) and each of those belongs to a supercategory (e.g. animal, vehicle).
+    # Category ID 1 is for Human, This will be 1 if they are labelled persons.
+    cat_dict_person = {"id": 1, "name": "person",
+                       "keypoints": key_body_labels,
+                       "skeleton": [[16, 14], [14, 12], [17, 15], [15, 13], [12, 13], [6, 12], [7, 13], [6, 7], [6, 8], [7, 9], [8, 10], [9, 11], [2, 3], [1, 2], [1, 3], [2, 4], [3, 5], [4, 6], [5, 7]],
+                       "supercategory": "person"}
+    coco_dict["categories"].append(cat_dict_person)
     cat_name2id = {}
+    this_cat_id = 2
+    labels = meta.find("task").find("labels")
     for label in labels.findall("label"):
         label_name = label.find("name").text
         if label_name in key_body_labels:
@@ -100,17 +92,14 @@ def main(argv):
         this_cat_id += 1
         coco_dict["categories"].append(cat_dict)
 
-    cat_dict = {"id": 1, "name": "person",
-                "keypoints": key_body_labels,
-                "skeleton": [[16, 14], [14, 12], [17, 15], [15, 13], [12, 13], [6, 12], [7, 13], [6, 7], [6, 8], [7, 9], [8, 10], [9, 11], [2, 3], [1, 2], [1, 3], [2, 4], [3, 5], [4, 6], [5, 7]],
-                "supercategory": "person"}
-    coco_dict["categories"].append(cat_dict)
-
+    # Add image info to JSON file
+    # The “images” section contains the complete list of images in the dataset.
+    # It's a list of images and information about each one.
+    img_idx2id = {}
     if img_root is not None:
         img_root = path(img_root, is_dir=True)
         img_paths = get_imgs_from_dir(img_root)
-        this_img_id = 1
-        img_idx2id = {}
+        this_img_id = 0
         for i, img_path in enumerate(img_paths):
             w, h = Image.open(img_path).size
             img_idx2id[i] = this_img_id
@@ -125,115 +114,155 @@ def main(argv):
 
     this_annot_id = 1
 
-    group_ids = []
+    # All shapes (points, bboxes) from one person belong to one group
+    xml_person_ids = []
     for track_elem in root.findall("track"):
         if ('group_id' in track_elem.attrib):
-            group_id = int(track_elem.attrib["group_id"])
-            if group_id not in group_ids:
-                group_ids.append(group_id)
+            xml_person_id = int(track_elem.attrib["group_id"])
+            if xml_person_id not in xml_person_ids:
+                xml_person_ids.append(xml_person_id)
 
-    if len(group_ids) == 0:
+    # No groups are set (in case of only person bboxes in XML), use track ID XML
+    if len(xml_person_ids) == 0:
         for track_elem in root.findall("track"):
             if ('id' in track_elem.attrib):
-                group_id = int(track_elem.attrib["id"])
-                if group_id not in group_ids:
-                    group_ids.append(group_id)
+                xml_person_id = int(track_elem.attrib["id"])
+                label = str(track_elem.attrib["label"])
+                if label.lower() == "person":
+                    xml_person_ids.append(xml_person_id)
 
-    for group_id in group_ids:
+    # Loop through XML according to ID (ID indicates shapes belonging to a specific unique person)
+    for xml_person_id in xml_person_ids:
         for frame_index in range(start_frame, stop_frame):
             key_points = []
-            prev_loc = []
-            for label_index in range(17):
+            prev_pos = []
+            # Iterate over all body parts
+            for body_label_idx in range(len(key_body_labels)):
                 is_point_available = False
                 for track_elem in root.findall("track"):
-                    if (not ('group_id' in track_elem.attrib)) or int(track_elem.attrib["group_id"]) != group_id:
+                    # if group ID different => not the person to convert in this loop
+                    if (not ('group_id' in track_elem.attrib)) or int(track_elem.attrib["group_id"]) != xml_person_id:
                         continue
-                    if key_body_labels[label_index] != track_elem.attrib["label"]:
+                    # if body label different => not the body part to convert in this loop
+                    if key_body_labels[body_label_idx] != track_elem.attrib["label"]:
                         continue
-                    tid = int(track_elem.attrib["id"])
-                    #catid = cat_name2id[track_elem.attrib["label"]]
+
                     for point_elem in track_elem.findall("points"):
+                        #  if frame ID different => not the point to convert in this loop
+                        point_frame_id = int(point_elem.attrib["frame"])
+                        if int(point_frame_id) != frame_index:
+                            continue
+
+                        # Indicates visibility— 0: outside, 1: labeled but not visible, and 2: labeled and visible
                         if bool(int(point_elem.attrib["outside"])):
                             visibil = 0
-                        else:
+                        elif bool(int(point_elem.attrib["occluded"])):
                             visibil = 1
-                        frame_id = int(point_elem.attrib["frame"])
-                        if int(frame_id) != frame_index:
-                            continue
-                        outside = int(point_elem.attrib["outside"])
-                        occluded = bool(int(point_elem.attrib["occluded"]))
-                        keyframe = bool(int(point_elem.attrib["keyframe"]))
+                        else:
+                            visibil = 2
+
+                        # [x1,y1,v1,x2,y2,v2...], → x and y indicate pixel positions in the image
                         points = point_elem.attrib["points"]
-                        locArr = points.split(',')
+                        pos_arr = points.split(',')
                         key_point = []
-                        for loc in locArr:
-                            key_point.append(float(loc))
+                        for pos in pos_arr:
+                            key_point.append(float(pos))
                         key_point.append(visibil)
                         key_points.extend(key_point)
-                        prev_loc = locArr
+                        prev_pos = pos_arr
                         is_point_available = True
-                if not is_point_available and len(prev_loc) > 0:
+                if not is_point_available and len(prev_pos) > 0:
                     key_point = []
-                    for loc in prev_loc:
-                        key_point.append(float(loc))
+                    for pos in prev_pos:
+                        key_point.append(float(pos))
                     key_point.append(0)
                     key_points.extend(key_point)
 
-            if img_root is None:
-                img_id = 0
-            else:
-                img_id = img_idx2id[frame_index - start_frame]
+            # Convert bbox person
             for track_elem in root.findall("track"):
-                if (('group_id' in track_elem.attrib) and int(track_elem.attrib["group_id"]) != group_id):
-                    continue
-                if (('group_id' not in track_elem.attrib) and int(track_elem.attrib["id"]) != group_id):
-                    continue
-                tid = int(track_elem.attrib["id"])
                 label_elem = track_elem.attrib["label"]
                 label_name = str(label_elem)
-                if label_name in key_body_labels:
+                if label.lower() != "person":
                     continue
-                catid = cat_name2id[label_elem]
+                # group ID different => not the person to convert in this loop
+                if (('group_id' in track_elem.attrib) and int(track_elem.attrib["group_id"]) != xml_person_id):
+                    continue
+                if (('group_id' not in track_elem.attrib) and int(track_elem.attrib["id"]) != xml_person_id):
+                    continue
                 for box_elem in track_elem.findall("box"):
+                    #  if frame ID different => not the bbox to convert in this loop
                     if int(box_elem.attrib["frame"]) != frame_index:
                         continue
                     if bool(int(box_elem.attrib["outside"])):
                         continue
                     frame_idx = int(box_elem.attrib["frame"])
-                    #imgid = img_idx2id[frame_idx - start_frame]
+
+                    if img_root is not None:
+                        imgid = img_idx2id[frame_idx - start_frame]
+                    else:
+                        imgid = 0
+
                     occluded = bool(int(box_elem.attrib["occluded"]))
                     keyframe = bool(int(box_elem.attrib["keyframe"]))
-                    l = float(box_elem.attrib["xtl"])
-                    t = float(box_elem.attrib["ytl"])
+                    # [x,y,width,height], → Denoting the bbox location of that person. Box coordinates are measured from the top left image corner and are 0-indexed<br />
+                    x = float(box_elem.attrib["xtl"])
+                    y = float(box_elem.attrib["ytl"])
                     r = float(box_elem.attrib["xbr"])
                     b = float(box_elem.attrib["ybr"])
-                    w = r - l
-                    h = b - t
+                    w = r - x
+                    h = b - y
+                    # The person's actions which are captured
                     actions = []
                     for attr_elem in box_elem.findall("attribute"):
+                        # attr_elem.text.lower()
                         if str(attr_elem.text) == "true" or str(attr_elem.text) == "True":
                             actions.append(attr_elem.attrib["name"])
                     annot_dict = {
                         "id": this_annot_id,
-                        "image_id": img_id,
+                        "image_id": imgid,
                         "frame_id": frame_idx,
                         "category_id": 1,
                         "keypoints": key_points,
-                        "bbox": [l, t, r, b],
+                        "bbox": [x, y, r, b],
                         # "bbox": [l, t, w, h],
-                        "area": w * h,
-                        "iscrowd": 0,
-                        "track_id": group_id,
-                        "attributes": {
-                            "occluded": occluded,
-                            "keyframe": keyframe,
-                            "activity": actions
-                        },
+                        #"area": w * h,
+                        #Is not a crowd (meaning it’s a single object)
+                        #"iscrowd": 0,
+                        "track_id": xml_person_id,
+                        "occluded": occluded,
+                        "keyframe": keyframe,
+                        "activity": actions
                     }
                     this_annot_id += 1
                     coco_dict["annotations"].append(annot_dict)
     write_json(out_json, coco_dict)
 
 
+def parse_args():
+    """Parse arguments of command line"""
+    parser = argparse.ArgumentParser(
+        description='Convert CVAT XML annotations to COCO JSON format'
+    )
+    parser.add_argument(
+        '--cvat-xml', metavar='FILE', required=True,
+        help='input file with CVAT annotation in xml format'
+    )
+    parser.add_argument(
+        '--image-dir', metavar='DIRECTORY', required=False,
+        help='directory which contains original images'
+    )
+    parser.add_argument(
+        '--coco', metavar='FILE', required=True,
+        help='FILE for output annotations in JSON format'
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    convert(args.cvat_xml, args.image_dir, args.coco)
+
+
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
