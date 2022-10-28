3a9eeaedb09afbbb7546285abf5b31ecc82c173a
Reggievw <reggievanwoensel@yahoo.com>
2022-10-28 11:58:29 +0200

    clean up code

--- cvatxml2coco.py
+++ cvatxml2coco.py
@@ -1,4 +1,5 @@
 """
+@Author Reginald Van Woensel
 Convert from CVAT XML to COCO JSON
 """
 
@@ -9,21 +10,21 @@ import sys
 
 from datetime import datetime, date
 from pathlib import Path
-from PIL import Image
 
+# bodykeypoints
+key_body_labels = [ "nose", "left_eye", "right_eye", "left_ear", "right_ear", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle" ]
+face_body_labels = [ "nose", "left_eye", "right_eye", "left_ear", "right_ear"]
 
 def write_json(json_path, dic):
     with open(json_path, "w") as f:
         json.dump(dic, f, indent=2)
     print(f"Wrote json to {json_path}")
 
-def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummyActions):
+def convert(xmlfile, jsonfile, onlyBoxes, withBodyKeyPoints, addDummyActions):
     if not withBodyKeyPoints and not onlyBoxes:
         print('--withBodyKeyPoints or --onlyBoxes is not set, one of those argument must be set ')
         exit(0)
-    """
-    Transforms an XML in CVAT format to COCO.
-    """
+
     # Parse XML
     print(f"Parse XML {xmlfile}")
     tree = ET.parse(xmlfile)
@@ -54,16 +55,14 @@ def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummy
         json_file_name = taskname if taskname else Path(xmlfile).stem
         out_json = f"{json_file_name}.json"
 
-    # A collection of “info”, “images”, “annotations”, “categories”
+    # A collection of “info”, “annotations”, “categories”
     coco_dict = {
         "info": {"description": taskname, "data_created": date_str},
         "categories": [],
         "annotations": [],
+        "licenses": [] #TODO check with geert@autimatic.be
     }
 
-    # bodykeypoint labels19
-    key_body_labels = [ "nose", "left_eye", "right_eye", "left_ear", "right_ear", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle" ]
-    face_body_labels = [ "nose", "left_eye", "right_eye", "left_ear", "right_ear"]
     # The “categories” object contains a list of categories (e.g. dog, boat) and each of those belongs to a supercategory (e.g. animal, vehicle).
     # Category ID 1 is for Human.
     cat_dict_person = {"id": 1, "name": "person",
@@ -73,10 +72,10 @@ def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummy
                         [8, 10], [1, 2], [0, 1], [0, 2], [1, 3], [2, 4],
                         [3, 5], [4, 6]],
                        "supercategory": "person"}
-
     coco_dict["categories"].append(cat_dict_person)
 
     cat_name2id = {}
+    # Other labels than person or keypoints
     this_cat_id = 2
     labels = meta.find("task").find("labels")
     for label in labels.findall("label"):
@@ -88,35 +87,14 @@ def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummy
         cat_name2id[label_name] = this_cat_id
         cat_dict = {"id": this_cat_id, "name": label_name, "supercategory": ""}
         this_cat_id += 1
-        #coco_dict["categories"].append(cat_dict)
-
-    # Add image info to JSON file
-    # The “images” section contains the complete list of images in the dataset.
-    # It's a list of images and information about each one.
-    img_idx2id = {}
-    if img_root is not None:
-        img_root = path(img_root, is_dir=True)
-        img_paths = get_imgs_from_dir(img_root)
-        img_id = 0
-        for i, img_path in enumerate(img_paths):
-            w, h = Image.open(img_path).size
-            img_idx2id[i] = img_id
-            img_dict = {
-                "id": img_id,
-                "file_name": str(img_path.relative_to(img_root)),
-                "height": h,
-                "width": w,
-            }
-            img_id += 1
-            coco_dict["images"].append(img_dict)
+        coco_dict["categories"].append(cat_dict)
 
     this_annot_id = 1
-    # All shapes (points, bboxes) from one person belong to one group
     xml_person_ids = [0]
-
     isGroupAvailable = False
-    if withBodyKeyPoints or withDummyActions:
+    if withBodyKeyPoints or addDummyActions:
         for track_elem in root.findall("track"):
+            # All shapes (points, bboxes) from one person belong to one group
             if ('group_id' in track_elem.attrib):
                 xml_person_id = int(track_elem.attrib["group_id"])
                 isGroupAvailable = True
@@ -136,10 +114,10 @@ def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummy
                     xml_person_ids.append(xml_person_id)
 
     print("Person tracks to process, %s" % len(xml_person_ids))
-    if len(xml_person_ids) > 500:
+    if len(xml_person_ids) > 999:
         sys.exit("Too much person tracks to process, %s!" % len(xml_person_ids))
 
-    # Loop through XML according to ID (ID indicates shapes belonging to a specific unique person)
+    # Loop through XML according to person ID (ID indicates shapes belonging to a specific unique person)
     print("Loop through XML according to ID")
     for xml_person_id in xml_person_ids:
         xml_person_start_frame = stop_frame
@@ -164,66 +142,17 @@ def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummy
         for frame_index in range(xml_person_start_frame, xml_person_stop_frame+1):
             key_points = []
             if withBodyKeyPoints:
-                prev_pos = []
-                # Iterate over all body parts
-                for body_label_idx in range(len(key_body_labels)):
-                    is_point_available = False
-                    for track_elem in root.findall("track"):
-                        # if group ID different => not the person to convert in this loop
-                        if ('group_id' in track_elem.attrib) and int(track_elem.attrib["group_id"]) != xml_person_id:
-                            continue
-                        if (not ('group_id' in track_elem.attrib)) and 0 != xml_person_id:
-                            continue
-                        # if body label different => no t the body part to convert in this loop
-                        if key_body_labels[body_label_idx] != track_elem.attrib["label"]:
-                            continue
-                        for point_elem in track_elem.findall("points"):
-                            #  if frame ID different => not the point to convert in this loop
-                            point_frame_id = int(point_elem.attrib["frame"])
-                            if int(point_frame_id) != frame_index:
-                                continue
-                            # Indicates visibility— 0: outside, 1: labeled but not visible, and 2: labeled and visible
-                            if bool(int(point_elem.attrib["outside"])):
-                                visibil = 0
-                            elif bool(int(point_elem.attrib["occluded"])):
-                                visibil = 1
-                            else:
-                                if track_elem.attrib["label"] in face_body_labels:
-                                    visibil = 1
-                                else:
-                                    visibil = 2
-                            # [x1,y1,v1,x2,y2,v2...], → x and y indicate pixel positions in the image
-                            points = point_elem.attrib["points"]
-                            pos_arr = points.split(',')
-                            key_point = []
-                            for pos in pos_arr:
-                                if visibil == 0:
-                                    key_point.append(float(0))
-                                else:
-                                    key_point.append(float(pos))
-                            key_point.append(visibil)
-                            key_points.extend(key_point)
-                            prev_pos = pos_arr
-                            is_point_available = True
-                    # if no point from xml
-                    if not is_point_available:
-                        key_point = []
-                        key_point.append(float(0))
-                        key_point.append(float(0))
-                        key_point.append(0)
-                        key_points.extend(key_point)
-                        is_point_available = True
-
-            if(withBodyKeyPoints and len(key_points) < 51):
-                print("Error length keypoints %s" %len(key_points))
+                convertBodyKeyPoints(root, key_body_labels, face_body_labels, xml_person_id, frame_index, key_points)
 
             # Convert bbox person
             for track_elem in root.findall("track"):
                 # group ID different => not the person to convert in this loop
                 if withBodyKeyPoints and ('group_id' in track_elem.attrib) and int(track_elem.attrib["group_id"]) != xml_person_id:
                     continue
+                # in the extracted XML for the first person the group_id could be empty
                 if withBodyKeyPoints and not ('group_id' in track_elem.attrib) and 0 != xml_person_id:
                     continue
+                # when the xml contains only boxes no group ids are provided
                 if onlyBoxes and  int(track_elem.attrib["id"]) != xml_person_id:
                     continue
                 for box_elem in track_elem.findall("box"):
@@ -237,13 +166,7 @@ def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummy
                     if bool(int(box_elem.attrib["outside"])):
                         continue
                     frame_idx = int(box_elem.attrib["frame"])
-                    if img_root is not None:
-                        imgid = img_idx2id[frame_idx - start_frame]
-                    else:
-                        imgid = 0
-
                     occluded = int(box_elem.attrib["occluded"])
-                    keyframe = bool(int(box_elem.attrib["keyframe"]))
                     # [x,y,width,height], → Denoting the bbox location of that person. Box coordinates are measured from the top left image corner and are 0-indexed<br />
                     x = float(box_elem.attrib["xtl"])
                     y = float(box_elem.attrib["ytl"])
@@ -253,23 +176,7 @@ def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummy
                     h = b - y
                     # The person's actions which are captured
                     actions = []
-                    if withDummyActions:
-                        for track_elem2 in root.findall("track"):
-                            if ("group_id" not in track_elem2.attrib or int(track_elem2.attrib["group_id"]) != xml_person_id):
-                                continue
-                            for points_elem in track_elem2.findall("points"):
-                                if int(points_elem.attrib["frame"]) != frame_index:
-                                    continue
-                                if bool(int(points_elem.attrib["outside"])):
-                                    continue
-                                for attr_elem in points_elem.findall("attribute"):
-                                    if attr_elem.text is not None and str(attr_elem.text.lower()) == "true":
-                                        text = attr_elem.attrib["name"]
-                                        actions.append(text)
-                    else:
-                        for attr_elem in box_elem.findall("attribute"):
-                            if attr_elem.text is not None and str(attr_elem.text.lower()) == "true":
-                                actions.append(attr_elem.attrib["name"])
+                    convertActions(addDummyActions, root, xml_person_id, box_elem, frame_index, actions)
                     annot_dict = {
                         "id": this_annot_id,
                         "frame_id": frame_idx,
@@ -281,26 +188,97 @@ def convert(xmlfile, img_root, jsonfile, onlyBoxes, withBodyKeyPoints, withDummy
                         "activity": actions
                     }
                     this_annot_id += 1
-                   # print("Added annotation for  ID %s" % this_annot_id)
                     coco_dict["annotations"].append(annot_dict)
                     #print("Processed, %s!, category %s" % (label_name, cat_name2id[label_name]))
 
-    # Convert other bboxes
+    # Convert other bboxes, index starting from the already converted person tracks
     used_track_ids = xml_person_ids
     coco_track_id = 1
     for track_elem in root.findall("track"):
         # generate new track_id
         while coco_track_id in used_track_ids:
             coco_track_id +=1
-        #track_id = int(track_elem.attrib["id"])
         for frame_index in range(start_frame, stop_frame):
-            convert_other_bboxes(img_root, start_frame, coco_dict, img_idx2id, this_annot_id, track_elem, coco_track_id, frame_index, cat_name2id)
+            convert_other_bboxes(coco_dict, this_annot_id, track_elem, coco_track_id, frame_index, cat_name2id)
         used_track_ids.append(coco_track_id)
         this_annot_id += 1
 
     write_json(out_json, coco_dict)
 
-def convert_other_bboxes(img_root, start_frame, coco_dict, img_idx2id, this_annot_id, track_elem, track_id, frame_index, cat_name2id):
+def convertActions(addDummyActions, root, xml_person_id, box_elem, frame_index, actions):
+    if addDummyActions:
+        for track_elem2 in root.findall("track"):
+            if ("group_id" not in track_elem2.attrib or int(track_elem2.attrib["group_id"]) != xml_person_id):
+                continue
+            for points_elem in track_elem2.findall("points"):
+                if int(points_elem.attrib["frame"]) != frame_index:
+                    continue
+                if bool(int(points_elem.attrib["outside"])):
+                    continue
+                for attr_elem in points_elem.findall("attribute"):
+                    if attr_elem.text is not None and str(attr_elem.text.lower()) == "true":
+                        text = attr_elem.attrib["name"]
+                        actions.append(text)
+    else:
+        for attr_elem in box_elem.findall("attribute"):
+            if attr_elem.text is not None and str(attr_elem.text.lower()) == "true":
+                actions.append(attr_elem.attrib["name"])
+
+def convertBodyKeyPoints(root, key_body_labels, face_body_labels, xml_person_id, frame_index, key_points):
+    #prev_pos = []
+    # Iterate over all body parts
+    for body_label_idx in range(len(key_body_labels)):
+        is_point_available = False
+        for track_elem in root.findall("track"):
+                        # if group ID different => not the person to convert in this loop
+            if ('group_id' in track_elem.attrib) and int(track_elem.attrib["group_id"]) != xml_person_id:
+                continue
+            if (not ('group_id' in track_elem.attrib)) and 0 != xml_person_id:
+                continue
+            # if body label different => no t the body part to convert in this loop
+            if key_body_labels[body_label_idx] != track_elem.attrib["label"]:
+                continue
+            for point_elem in track_elem.findall("points"):
+                #  if frame ID different => not the point to convert in this loop
+                point_frame_id = int(point_elem.attrib["frame"])
+                if int(point_frame_id) != frame_index:
+                    continue
+                # Indicates visibility— 0: outside, 1: labeled but not visible, and 2: labeled and visible
+                if bool(int(point_elem.attrib["outside"])):
+                    visibil = 0
+                elif bool(int(point_elem.attrib["occluded"])):
+                    visibil = 1
+                else:
+                    if track_elem.attrib["label"] in face_body_labels:
+                        visibil = 1
+                    else:
+                        visibil = 2
+                # [x1,y1,v1,x2,y2,v2...], → x and y indicate pixel positions in the image
+                points = point_elem.attrib["points"]
+                pos_arr = points.split(',')
+                key_point = []
+                for pos in pos_arr:
+                    if visibil == 0:
+                        key_point.append(float(0))
+                    else:
+                        key_point.append(float(pos))
+                key_point.append(visibil)
+                key_points.extend(key_point)
+                #prev_pos = pos_arr
+                is_point_available = True
+                # if no point from xml
+        if not is_point_available:
+            key_point = []
+            key_point.append(float(0))
+            key_point.append(float(0))
+            key_point.append(0)
+            key_points.extend(key_point)
+            is_point_available = True
+
+    if(len(key_points) < 51):
+        sys.exit("Error length keypoints %s" %len(key_points))
+
+def convert_other_bboxes(coco_dict, this_annot_id, track_elem, track_id, frame_index, cat_name2id):
     for box_elem in track_elem.findall("box"):
         label_elem = track_elem.attrib["label"]
         label_name = str(label_elem)
@@ -312,12 +290,6 @@ def convert_other_bboxes(img_root, start_frame, coco_dict, img_idx2id, this_anno
         if bool(int(box_elem.attrib["outside"])):
             continue
         frame_idx = int(box_elem.attrib["frame"])
-
-        if img_root is not None:
-            imgid = img_idx2id[frame_idx - start_frame]
-        else:
-            imgid = 0
-
         occluded = bool(int(box_elem.attrib["occluded"]))
         keyframe = bool(int(box_elem.attrib["keyframe"]))
         # [x,y,width,height], → Denoting the bbox location of that person. Box coordinates are measured from the top left image corner and are 0-indexed<br />
@@ -329,7 +301,6 @@ def convert_other_bboxes(img_root, start_frame, coco_dict, img_idx2id, this_anno
         h = b - y
         annot_dict = {
             "id": this_annot_id,
-            "image_id": imgid,
             "frame_id": frame_idx,
             "category_id": cat_name2id[label_name],
             "bbox": [x, y, round(w, 3), round(h, 3)],
@@ -362,13 +333,13 @@ def parse_args():
     parser.add_argument("--onlyBoxes", default=False, action="store_true",
                     help="Flag to use only body boxes")
 
-    parser.add_argument("--withDummyActions", default=False, action="store_true",
+    parser.add_argument("--addDummyActions", default=False, action="store_true",
                     help="Flag to use dummy actions to extract activity")
     return parser.parse_args()
 
 def main():
     args = parse_args()
-    convert(args.cvat_xml, args.image_dir, args.coco, args.onlyBoxes, args.withBodyKeyPoints, args.withDummyActions)
+    convert(args.cvat_xml, args.coco, args.onlyBoxes, args.withBodyKeyPoints, args.withDummyActions)
 
 if __name__ == '__main__':
     main()
